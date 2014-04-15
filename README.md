18340f9f912c71b66f09c67df21d4be6e94f000f
mcollie1 <marcus.collier7@yahoo.com>
2014-04-15 12:02:13 -0400

    First commit and README update

---
+++ README.md
@@ -0,0 +1,28 @@
+== README
+
+Blocmetrics - Build a general purpose analytics service that your customers can use to track events on their websites.
+
+Made with my mentor at [Bloc](http://bloc.io)
+
+Things you may want to cover:
+
+* Ruby version - 2.0.0p451
+
+* System dependencies
+
+* Configuration
+
+* Database creation
+
+* Database initialization
+
+* How to run the test suite
+
+* Services (job queues, cache servers, search engines, etc.)
+
+* Deployment instructions
+
+* ...
+
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blocmetrics::Application.config.secret_key_base = 'ee82f0f6aa17f58e894a7298c73d6a07e0627b7c29820d86f34b4f238a466805511628a892eedede60d7f89a5e1d598962c73bd03d751bfe4d35814abacb0dbf'
