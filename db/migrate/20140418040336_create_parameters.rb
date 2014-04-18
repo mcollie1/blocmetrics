d5915684971ce2401baa32d755a481987feb3c5b
mcollie1 <marcus.collier7@yahoo.com>
2014-04-18 17:23:35 -0400

    events table

--- db/migrate/20140418040336_create_parameters.rb
+++ db/migrate/20140418040336_create_parameters.rb
@@ -1,6 +1,7 @@
 class CreateParameters < ActiveRecord::Migration
   def change
     create_table :parameters do |t|
+      t.string :topic_name
       t.string :post_name
       t.integer :name
       t.references :event, index: true
