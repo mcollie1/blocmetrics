a30a5ffc3ed8febc27b165f0e20f23e45d32992d
mcollie1 <marcus.collier7@yahoo.com>
2014-04-18 17:33:26 -0400

    get route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Blocmetrics::Application.routes.draw do
 
-  resources :events, only: [:index]
+  match 'events' => "events#index", via: :get
 
   post :events, to: 'events#create'
   match 'events' => "events#index", via: :options
