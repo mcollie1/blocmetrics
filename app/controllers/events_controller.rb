086c7b815a7009f9b4e466497f58eda6b28f9bcc
mcollie1 <marcus.collier7@yahoo.com>
2014-04-18 21:47:35 -0400

    fixing template error

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,7 +4,7 @@ class EventsController < ApiController
 
   def index
     respond_to do |format|
-      format.html #index.html.erb
+      format.json { head :ok }
     end
     @events = Event.all
   end
