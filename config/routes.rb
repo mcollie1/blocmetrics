83ad2fd0a1433bf67e2dbf688ff1f88530510e0b
mcollie1 <marcus.collier7@yahoo.com>
2014-04-18 21:37:33 -0400

    changed routes to test

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Blocmetrics::Application.routes.draw do
 
+  match 'events' => "events#index", via: :get
+
   post :events, to: 'events#create'
   match 'events' => "events#index", via: :options
 end
