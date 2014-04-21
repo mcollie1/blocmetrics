601d7d3cd17b02dbca2070df10264b60e07ced5f
mcollie1 <marcus.collier7@yahoo.com>
2014-04-20 21:11:18 -0400

    strong parameters for nested association

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -43,6 +43,6 @@ class EventsController < ApiController
   end
 
   def parameter_params
-    params.require(:parameter).permit(:topic_name)
+    params.require(:event).permit(parameters_attributes: [:id, :topic_name])
   end
 end
