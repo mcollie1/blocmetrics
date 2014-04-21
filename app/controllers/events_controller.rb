bae49f039d997e6db425993ac20137f055e107bf
mcollie1 <marcus.collier7@yahoo.com>
2014-04-21 00:25:33 -0400

    strong parameters nested attributes

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -8,43 +8,17 @@ class EventsController < ApiController
 
   # POST /events.json
   def create
+    Event.create!(event_params)
+    
     respond_to do |format|
       format.json { head :ok }
     end
-
-    if Event.count > 0 # if there are application events?
-      event_name = params['name']
-      events = Event.all
-      names = events.map { |e| e["name"] }
-
-      if names.include?(event_name)
-        event = events.find_by_name(event_name)
-        event.parameters.create!(parameter_params)
-      else
-        new_event = Event.create!(event_params)
-        new_event.parameters.create!(parameter_params)
-      end
-    else #if no event exists yet
-      first_event = Event.create!(event_params)
-      first_event.parameters.create!(parameter_params)
-    end
-    #Parameters:
-      #{
-        #name: "event name; required",
-        #property_1: "some value",
-        #property_2: "some other value"
-      #}
   end
 
   private
 
   def event_params
-    params.require(:event).permit(:name, :created_at, :updated_at)
-  end
-
-  def parameter_params
-    puts "**** params.to_yaml"
-
-    params.require(:parameter).permit(:topic_name)
+    puts "**** #{params.to_yaml}"
+    params.require(:event).permit(:name, parameters_attributes: [:topic_name])
   end
 end
