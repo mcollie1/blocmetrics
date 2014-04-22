9b84bb88081e0b78b65a3142ea39fb842e829042
mcollie1 <marcus.collier7@yahoo.com>
2014-04-22 17:07:16 -0400

    ALTER TABLE integer, syntax

--- db/migrate/20140422164523_change_data_type_for_events_column.rb
+++ db/migrate/20140422164523_change_data_type_for_events_column.rb
@@ -1,9 +1,5 @@
 class ChangeDataTypeForEventsColumn < ActiveRecord::Migration
   def change
-    change_table :events do |t|
-      t.change :app_user, :integer
-    end
-
     change_column :events, :app_user, 'integer USING CAST(app_user AS integer)'
   end
   
